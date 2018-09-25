0422bd81ed9116b93c8b7e266cac9d1843cdee75
Iovescode <13022195532@126.com>
2018-09-25 23:51:46 +0800

    no message

--- gantt/gantt/src/components/Gantt.vue
+++ gantt/gantt/src/components/Gantt.vue
@@ -10,7 +10,7 @@
       <tr v-for="(items,indexs) in opt" :key='indexs'>
         <td v-for="(item, index) in newDate" :key='index'>
           <span v-if="index=='0'" >{{items.title}}</span>
-          <span v-else :data-x='item.day' :data-y='indexs+1'><a :style="{ background: item.hh.activeColor,width: item.hh.width}" @mouseenter="mouter" @mouseleave="moulea" v-if="item.day==item.hh.today&&item.hh.y==indexs"  class="iniClass">{{item.hh.title}}</a><a v-if="item.day==item.hh.today&&item.hh.y==indexs" @mouseenter="mouter" @mouseleave="moulea" class="iniClassB" :style="{ background: item.hh.activeColors,width: item.hh.widths}">{{item.hh.titles}}</a></span>
+          <span v-else :data-x='item.day' :data-y='indexs+1'><a :style="{ background: item.hh.activeColor,width: item.hh.width}" @mouseenter="mouter" @mouseleave="moulea" v-if="item.day==item.hh.today&&item.hh.y==indexs"  class="iniClass">{{item.hh.title}}</a><a v-if="item.day==item.hh.today&&item.hh.y==indexs" @mouseenter="mouter" @mouseleave="moulea" class="iniClassB" :style="{ background: item.hh.activeColors,width: item.hh.widths}">''</a></span>
         </td>
       </tr>
     </table>
@@ -24,17 +24,31 @@ export default {
   data () {
     return {
       opt: [
-        { title: '项目一', today: '2018-9-7' },
         {
           title: '项目二',
-          titles: '项目二',
           today: '2018-9-2',
+          endTime: '2018-9-9',
           activeColor: 'red',
           activeColors: 'green',
           width: '200px',
           widths: '300px'
         },
-        { title: '项目三', today: '2018-9-12' }
+        {
+          title: '项目二',
+          today: '2018-9-3',
+          activeColor: 'red',
+          activeColors: 'green',
+          width: '200px',
+          widths: '300px'
+        },
+        {
+          title: '项目二',
+          today: '2018-9-7',
+          activeColor: 'red',
+          activeColors: 'green',
+          width: '200px',
+          widths: '300px'
+        }
       ],
       opts: [],
       date: [],
@@ -66,28 +80,77 @@ export default {
         let hh = {}
         this.dates.map((itemss, indexss) => {
           if (items.day === itemss.hh.today) {
+            let datediffStarEnd = this.getweek(itemss.hh.endTime).nowTimes - this.getweek(itemss.hh.today).nowTimes
+            datediffStarEnd = datediffStarEnd / 86400
+            let datediffNow = this.getweek(new Date()).nowTimes - this.getweek(itemss.hh.today).nowTime
+            datediffNow = datediffNow / 86400
+            let datediff = datediffNow / datediffStarEnd
             hh = { ...itemss.hh }
+            hh.sum = datediff
+            console.log(datediff)
           }
         })
         let obj = { day: items.day, hh: hh }
+
         this.newDate.push(obj)
       })
     },
     // 鼠标事件
     mouter (e) {
-      $('.tooltip').css('top', 45)
+      // $('.tooltip').css('top', 45)
       this.isshow = true
+      console.log(this.getweek(new Date()))
     },
     moulea (e) {
       this.isshow = false
     },
-    // 工具函数
+    // 工具函数   时间
     mGetDate () {
       var date = new Date()
       var year = date.getFullYear()
       var month = date.getMonth() + 1
       var d = new Date(year, month, 0)
       return d.getDate()
+    },
+    DateDiff (sDate1, sDate2) {
+      var aDate, oDate1, oDate2, iDays
+      aDate = sDate1.split('-')
+      oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])
+      aDate = sDate2.split('-')
+      oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])
+      iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24)
+      return iDays
+    },
+    getweek (currentTime) {
+      var currentDate = new Date(currentTime)
+      var timesStamp = currentDate.getTime()
+      var currenDay = currentDate.getDay()
+      // var Times = new Date(currentTime.toLocaleDateString()).getTime()
+      var dates = []
+      for (var i = 0; i < 7; i++) {
+        const Dates = new Date(timesStamp + 24 * 60 * 60 * 1000 * (i - (currenDay + 6) % 7))
+        const timeHour = Dates.getFullYear() + '-' + (Dates.getMonth() + 1) + '-' + Dates.getDate() + ' ' + Dates.getHours() + ':' + Dates.getMinutes() + ':' + Dates.getSeconds()
+        dates.push(this.formatDate(timeHour))
+      }
+      if (currenDay === 0) {
+        currenDay = 7
+      }
+      return { week: dates, day: currenDay, nowTimes: timesStamp }
+    },
+    formatDate (time) {
+      var date = new Date(time)
+      var y = date.getFullYear()
+      var m = date.getMonth() + 1
+      m = m < 10 ? ('0' + m) : m
+      var d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      var h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      var minute = date.getMinutes()
+      var second = date.getSeconds()
+      minute = minute < 10 ? ('0' + minute) : minute
+      second = second < 10 ? ('0' + second) : second
+      return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second
     }
   }
 }
